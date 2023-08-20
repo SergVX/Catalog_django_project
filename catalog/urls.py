f268b82267e8659a1b9e53f8c515e16f40a8553d
SergVX <s3rgvx@gmail.com>
2023-08-21 01:12:38 +0400

    Обновлена страница блогов, внесены изменения в urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from catalog.views import ContactsListView, ProductListView, ProductDetailView
+from catalog.views import ContactsListView, ProductListView, ProductDetailView, BlogList
 
 app_name = 'catalog'
 
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', ContactsListView.as_view(), name='contacts'),
     path('<slug:pk>/product', ProductDetailView.as_view(), name='card_product'),
-    # path('blog/', BlogList.as_view(), name='blog'),  # http://127.0.0.1:8000/blog/  Список статей блога
+    path('blog/', BlogList.as_view(), name='blog'),  # http://127.0.0.1:8000/blog/  Список статей блога
     # path('blog/<slug:slug>/', BlogDetailView.as_view(), name='blog_post'),  # http://127.0.0.1:8000/blog/<slug>
     # path('create_post/', BlogCreatePost.as_view(), name='create_post'),  # http://127.0.0.1:8000/create_post/
     # path('update_post/<slug:slug>/', BlogUpdatePost.as_view(), name='update_post'), # http://127.0.0.1:8000/update_post/
